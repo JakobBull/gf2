c8c9cc898705de6a7caa7a7fe0b0c1c9683fd5b4
aa2082 <aa2082@cam.ac.uk>
2021-05-29 12:52:39 +0100

    trying to fix parser

--- logsim/parse.py
+++ logsim/parse.py
@@ -33,12 +33,12 @@ class Parser:
     parse_network(self): Parses the circuit definition file.
     """
 
-    def __init__(self, names, devices, network, monitors, scanner):
+    def __init__(self, names, scanner):
         """Initialise constants."""
         self.names = names
-        self.devices = devices
-        self.network = network
-        self.monitors = monitors
+        #self.devices = devices
+        #self.network = network
+        #self.monitors = monitors
         self.scanner = scanner
 
         self.type = None
@@ -59,41 +59,32 @@ class Parser:
         self.setclock_parsed = False
         self.monitor_parsed = False
 
-        
-
-
-
-        
-
     def parse_network(self):
         """Parse the circuit definition file."""
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
 
-        
-
         while True:
-
             # Call for the next symbol from scanner
             self.symbol = self.scanner.get_symbol()
-            
-            if self.symbol.type == self.scanner.HEADING: #Check if symbol is a Heading
+
+            if self.symbol.id>=scanner.CONNECTIONS_ID and self.symbol.id<=scanner.MONITOR_ID #Check if symbol is a Heading
                 if self.headings.index(self.symbol) != self.headings_found: #Check if headings are called in the right order
                     self.parse_errors += 1
                     raise SyntaxError("Headings called in the wrong order")
-                    
+
                 elif self.symbol.id == self.scanner.NETWORK_ID:
                     self.headings_found += 1
 
-                
+
                 elif self.symbol.id == self.scanner.DEVICES_ID:
                     if self.sections_complete == 0 and self.headings_found == 1:
                         self.headings_found += 1
                         self.device_list()
                     else:
                         raise SyntaxError("Headings called in wrong order")
-                    
+
 
                 elif self.symbol == self.scanner.CONNECTIONS_ID:
                     if self.sections_complete == 1 and self.headings_found == 2:
@@ -108,7 +99,7 @@ class Parser:
                         self.signal_list()
                     else:
                         raise SyntaxError("Headings called in wrong order")
-                    
+
 
                 elif self.symbol == self.scanner.MONITOR_ID:
                     if self.sections_complete == 5 and self.headings_found == 6:
@@ -117,12 +108,12 @@ class Parser:
                     else:
                         raise SyntaxError("Headings called in wrong order")
 
-                
+
             elif self.sections_complete == 6 and self.headings_found == 7:
                 return True
             else:
                 return False
-                
+
 
 
     def OPENCURLY_search(self):
@@ -130,12 +121,12 @@ class Parser:
         self.symbol = self.scanner.get_symbol()
         if self.symbol.type != self.scanner.LEFT_BRACKET:
             raise SyntaxError("Always need to follow a heading with {")
-              
+
     def device_list(self):
         """ Function which parses the device list"""
 
         self.OPENCURLY_search()
-        
+
         self.symbol = self.scanner.get_symbol()
         self.device_parse()
         self.symbol = self.scanner.get_symbol()
@@ -147,19 +138,19 @@ class Parser:
                 break
             else:
                 self.device_parse()
-                self.symbol = self.scanner.get_symbol               
-               
+                self.symbol = self.scanner.get_symbol
+
     def device_parse(self):
         """Function which parses a single line of a device definition"""
         # Expected format : name EQUALS device
 
         if self.symbol.type != self.scanner.NAME:
             self.parse_errors += 1
-            raise SyntaxError("Name of device must contain a letter") 
-        
+            raise SyntaxError("Name of device must contain a letter")
+
         else:
             self.device_names.append(self.symbol) # add symbol id to a list of device ids
-            self.symbol = self.scanner.get_symbol() #Get next symbol which should be an = sign   
+            self.symbol = self.scanner.get_symbol() #Get next symbol which should be an = sign
             if self.symbol.type != self.scanner.EQUALS:
                     self.parse_errors += 1
                     raise SyntaxError
@@ -168,28 +159,28 @@ class Parser:
                 if self.symbol.id < 2 and self.symbol.id > 9:
                     self.parse_errors += 1
                     raise SyntaxError("Device type not found")
-                
-                    
+
+
 
     def connection_list(self):
         """Function whhich parses the connection list"""
-        
+
         self.OPENCURLY_search()
 
         self.symbol = self.scanner.get_symbol() # Go to first symbol of line
-        self.connection_parse() 
+        self.connection_parse()
         self.symbol = self.scanner.get_symbol() # Go to last symbol of line, should be ;
         while self.symbol.type == self.scanner.SEMICOLON:
             self.symbol = self.scanner.get_symbol() # Go to first symbol of next line
             if self.symbol.type == self.scanner.LEFT_BRACKET: # Check if } which denotes end of connections
                 self.connections_parsed = True
-                self.sections_complete += 1 
+                self.sections_complete += 1
                 break
             else:
-                self.connection_parse() 
+                self.connection_parse()
                 self.symbol = self.scanner.get_symbol  #Go to last symbol of line, should be ;
 
-                
+
     def connection_parse(self):
         """Function which parses a single connection"""
         # Expected format : name HYPHEN name DOT INPUT
@@ -215,7 +206,7 @@ class Parser:
                     if self.symbol.type != self.scanner.PERIOD:
                         raise SyntaxError("No . found")
 
-                    else: 
+                    else:
                         self.symbol = self.scanner.get_symbol()
                         if self.symbol.type != self.scanner.NUMBER:
                             raise SyntaxError("Input not defined")
@@ -235,19 +226,19 @@ class Parser:
         else:
             self.parse_errors += 1
             raise SyntaxError("SETSIGNAL heading expected")
-        
+
         self.symbol = self.scanner.get_symbol() #Get next symbol, should be SETCLOCK
         if self.symbol == self.scanner.SETCLOCK_ID:
             self.headings_found += 1
             self.setclock_parse()
-        
+
         self.symbol = self.scanner.get_symbol() #Get next symbol, should be }
         if self.symbol.type == self.scanner.RIGHT_BRACKET:
             self.sections_complete += 1
-        
 
 
-    
+
+
     def setsignal_list(self):
         """Function which parses the setsignal section"""
 
@@ -256,32 +247,32 @@ class Parser:
         if self.symbol.type == self.scanner.RIGHT_BRACKET:
             self.setsignal_parsed = True
             self.sections_complete += 1
-            return 
+            return
         self.setsignal_parse()
         while self.symbol.type == self.scanner.SEMICOLON:
             self.symbol = self.scanner.get_symbol() # Go to first symbol of next line
             if self.symbol.type == self.scanner.RIGHT_BRACKET: # Check if } which denotes end of setsignal
                 self.setsignal_parsed = True
                 self.sections_complete += 1
-                break 
+                break
             self.setsignal_parse()
-            
-        
+
+
 
 
     def setsignal_parse(self):
         """Function which parses a single line of the setsignal section"""
         #Expected format : name EQUALS BINARYNUMBER "starttime" NUMBER SEMICOLON
-        
-        
+
+
 
         if self.symbol not in self.device_names:
             self.parse_errors += 1
             raise SyntaxError("Device not defined")
-            
-        
-        self.symbol = self.scanner.get_symbol() 
-        
+
+
+        self.symbol = self.scanner.get_symbol()
+
         if self.symbol.type != self.scanner.EQUALS:
                 self.parse_errors += 1
                 raise SyntaxError("= sign expected")
@@ -298,7 +289,7 @@ class Parser:
         if self.symbol != "starttime":
             self.parse_errors += 1
             raise SyntaxError("word starttime expected")
-        
+
         self.symbol = self.scanner.get_symbol()
 
         if self.symbol.type != self.scanner.NUMBER:
@@ -318,14 +309,14 @@ class Parser:
         if self.symbol.type == self.scanner.RIGHT_BRACKET:
             self.setclockparsed = True
             self.sections_complete += 1
-            return 
+            return
         self.setclock_parse()
         while self.symbol.type == self.scanner.SEMICOLON:
             self.symbol = self.scanner.get_symbol() # Go to first symbol of next line
             if self.symbol.type == self.scanner.RIGHT_BRACKET: # Check if } which denotes end of setsignal
                 self.setclock_parsed = True
                 self.sections_complete += 1
-                break 
+                break
             self.setclock_parse()
 
 
@@ -333,18 +324,18 @@ class Parser:
     def setclock_parse(self):
         """"Function which parses a single line of the setclock section"""
         # Expected format : name EQUALS BINARYNUMBER "starttime" NUMBER "period" NUMBER "first change" NUMBER SEMICOLON
-        
-       
+
+
         if self.symbol not in self.device_names:
             self.parse_errors += 1
             raise SyntaxError("Device not defined")
 
-        self.symbol = self.scanner.get_symbol() 
-        
+        self.symbol = self.scanner.get_symbol()
+
         if self.symbol.type != self.scanner.EQUALS:
                 self.parse_errors += 1
                 raise SyntaxError("= sign expected")
-        
+
         self.symbol = self.scanner.get_symbol()
         if self.symbol.id != 0 and self.symbol.id != 1 and self.symbol.type == self.scanner.NUMBER:
             self.parse_errors += 1
@@ -357,9 +348,9 @@ class Parser:
         if self.symbol.id != self.scanner.starttime_ID:
             self.parse_errors += 1
             raise SyntaxError("word starttime expected")
-        
+
         self.symbol = self.scanner.get_symbol()
-        
+
         if self.symbol.type != self.scanner.NUMBER:
             self.parse_errors += 1
             raise SyntaxError("Integer number required")
@@ -369,9 +360,9 @@ class Parser:
         if self.symbol.type != self.scanner.period_ID:
             self.parse_errors += 1
             raise SyntaxError("word period expected")
-        
+
         self.symbol = self.scanner.get_symbol()
-        
+
         if self.symbol.type != self.scanner.NUMBER:
             self.parse_errors += 1
             raise SyntaxError("Integer number required")
@@ -387,9 +378,9 @@ class Parser:
         if self.symbol != "symbol":
             self.parse_errors += 1
             raise SyntaxError("word symbol expected")
-        
+
         self.symbol = self.scanner.get_symbol()
-        
+
         if self.symbol.type != self.scanner.NUMBER:
             self.parse_errors += 1
             raise SyntaxError("Integer number required")
@@ -411,7 +402,7 @@ class Parser:
         if self.symbol.type == self.scanner.RIGHTBRACKET:
             self.parse_errors += 1
             raise SyntaxError("No devices monitored")
-        
+
         else:
             while self.symbol.type == self.scanner.SEMICOLON:
                 self.symbol = self.scanner.get_symbol()
@@ -422,68 +413,15 @@ class Parser:
                 else:
                     self.monitor_parse()
 
-
-    
     def monitor_parse(self):
         """Function which parses a line in Monitor """
         #Expected format : name SEMICOLON
         if self.symbol not in self.device_names:
             self.parse_errors += 1
             raise SyntaxError("Device not defined")
-        
+
         self.symbol = self.scanner.get_symbol()
 
         if self.symbol.type != self.scanner.SEMICOLON:
             self.parse_errors += 1
             raise SyntaxError("Expected ; to end line")
-
-
-
-
-
-        
-
-
-            
-                
-        
-        
-
-
-        
-        
-        
-
-
-
-
-                
-            
-     
-
-
-                        
-
-
-
-            
-        
-
-                            
-
-
-
-                
-
-
-
-                
-
-
-            
-
-
-
-
-
-            
