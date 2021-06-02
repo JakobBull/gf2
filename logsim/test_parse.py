e0246b179b8564313e36e657c9bd3e4b198ec68a
aa2082 <aa2082@cam.ac.uk>
2021-06-02 13:06:39 +0100

    adjusted test_parse.py to see if parser detects multiple errors and confirms they are the right type at the right symbol.

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -16,20 +16,30 @@ def test_working_spec():
     assert True
 
 @pytest.mark.parametrize(
-    "file, expected_string",
+    "file, expected_errors",
+    # expected_errors, list of list of properties for each error that should occur
+    # each error will take a list of the form:
+    # error_type, symbol_string_that_caused_error, line_number, start_char_number
+    # can determine line_number and start_char_number from text editor like
+    # atom/vs code for writing tests. Usually at the bottom in the form:
+    # line# : char#
     [
-        ("test_1", Error.error_message[0]),
-        ("test_2", Error.error_message[1]),
-        ("test_3", Error.error_message[2]),
-        ("test_4", Error.error_message[3]),
+        ("test_1", [[0,"CONNECTIONS",1,1],[3,"-",3,9]])
     ],
 )
 # tests if query returns correct name ID for a name sting
-def test_query_output(file, expected_string):
+def test_error_detection(file, expected_errors):
     path = "parse_test_files/"
     path += file
     path += ".txt"
     scan = Scanner(path, Names())
     parse = Parser(Names(), scan)
-    with pytest.raises(SyntaxError, match= expected_string):
-        parse.parse_network()
+    parse.parse_network()
+    for i in range(len(expected_errors)):
+        error = expected_errors[i] # the list for the ith error from expected_errors
+        # make sure error types stored in error class align with those expected
+        # by expected_errors
+        assert(Error.types[i] == error[0])
+        assert(Error.symbols[i].string == error[1])
+        assert(Error.symbols[i].line_number == error[2])
+        assert(Error.symbols[i].start_char_number == error[3])
