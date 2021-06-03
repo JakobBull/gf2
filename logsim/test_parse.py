3139e800dd5d731805ff4c7995fae8b44cf6bf7a
aa2082 <aa2082@cam.ac.uk>
2021-06-03 16:48:49 +0100

    make pep8 pep257

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -11,28 +11,31 @@ import pytest
 import sys
 import os
 
+
 @pytest.mark.parametrize(
     "file, expected_errors",
-    # expected_errors, list of list of properties for each error that should occur
-    # each error will take a list of the form:
-    # error_type, symbol_string_that_caused_error, line_number, start_char_number
+    # expected_errors, list of list of properties for each error that
+    # should occur.
+    # Each error will take a list of the form:
+    # error_type, symbol_string_that_caused_error, line_number,
+    # start_char_number
     # can determine line_number and start_char_number from text editor like
     # atom/vs code for writing tests. Usually at the bottom in the form:
     # line# : char#
     [
-        ("no_errors", []),
-        ("test_1",[[0,"CONNECTIONS",1,1]]),
-        ("test_2",[[1,"SW1",3,5]]),
-        ("test_3",[[2,"912",4,5]]),
-        ("test_4",[[4,"-",6,8]]),
-        ("test_5",[[13,"1",9,14]]),
-        ("test_6",[[12,";",10,15]]),
-        ("test_7",[[11,"G2.I1",11,8]]),
-        ("test_8",[[10,"G22",12,5]]),
-        ("test_9",[[18,"SW19",15,5]]),
-        ("test_10",[[19,"0",16,9]]),
-        ("test_11",[[27,"W2",20,5]]),
-        ("test_12",[[24,"W2",20,5]]),
+        ("no_errors",  []),
+        ("test_1", [[0, "CONNECTIONS", 1, 1]]),
+        ("test_2", [[1, "SW1", 3, 5]]),
+        ("test_3", [[2, "912", 4, 5]]),
+        ("test_4", [[4, "-", 6, 8]]),
+        ("test_5", [[13, "1", 9, 14]]),
+        ("test_6", [[12, ";", 10, 15]]),
+        ("test_7", [[11, "G2.I1", 11, 8]]),
+        ("test_8", [[10, "G22", 12, 5]]),
+        ("test_9", [[18, "SW19", 15, 5]]),
+        ("test_10", [[19, "0", 16, 9]]),
+        ("test_11", [[27, "W2", 20, 5]]),
+        ("test_12", [[24, "W2", 20, 5]]),
     ]
     )
 # tests if query returns correct name ID for a name sting
@@ -51,7 +54,8 @@ def test_error_detection(file, expected_errors):
     parser.parse_network()
     # print(len(Error.types))
     for i in range(len(expected_errors)):
-        error = expected_errors[i] # the list for the ith error from expected_errors
+        error = expected_errors[i]
+        # the list for the ith error from expected_errors
         # make sure error types stored in error class align with those expected
         # by expected_errors
         # print("1:", end="\t")
